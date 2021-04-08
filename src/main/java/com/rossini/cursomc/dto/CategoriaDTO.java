5d9dfacfa11a02d7bb4135f3684fa0c013a1482f
Rossini <rossyny18@gmail.com>
2021-04-07 21:45:44 -0300

    PUT, GET, DELETE e validações

--- src/main/java/com/rossini/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/rossini/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.rossini.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.rossini.cursomc.domain.Categoria;
 
 //Data Transfer Object - esta classe serve para buscar somente o necessário do BD
@@ -10,6 +14,9 @@ public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/rossini/cursomc/resources/ClienteResource.java
+++ src/main/java/com/rossini/cursomc/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package com.rossini.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rossini.cursomc.domain.Cliente;
+import com.rossini.cursomc.dto.ClienteDTO;
 import com.rossini.cursomc.services.ClienteService;
 
 @RestController
@@ -24,5 +33,44 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(obj);
 		
 	}
+	
+	//Método para atualizar
+		@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+		public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
+			Cliente obj = service.fromDTO(objDto);
+			obj.setId(id);
+			obj = service.update(obj);
+			return ResponseEntity.noContent().build();
+		}
+		
+		//metodo para deletar
+		@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+		public ResponseEntity<Void> delete(@PathVariable Integer id) {
+			service.deleteById(id);
+			return ResponseEntity.noContent().build();
+		}
+		
+		//esse método busca todas as categorias
+		//foi usado o DTO para buscar somete as categorias sem seus produtos
+		@RequestMapping(method = RequestMethod.GET)
+		public ResponseEntity<List<ClienteDTO>> findAll() {
+			List<Cliente> list = service.findAll();
+			List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+			return ResponseEntity.ok().body(listDto);
+			
+		}
+		
+		/*Método para fazer paginação usando parametros*/
+		@RequestMapping(value="/page", method = RequestMethod.GET)
+		public ResponseEntity<Page<ClienteDTO>> findPage(
+				@RequestParam(value="page", defaultValue="0") Integer page,
+				@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+				@RequestParam(value="orderBy", defaultValue="nome") String orderBy,
+				@RequestParam(value="direction", defaultValue="ASC") String direction) {
+			Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+			Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+			return ResponseEntity.ok().body(listDto);
+			
+		}
 	
 }
