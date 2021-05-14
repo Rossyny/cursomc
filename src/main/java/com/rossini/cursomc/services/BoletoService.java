cfcce984b17cd500840211efa90517f2ab34617a
Rossini <rossyny18@gmail.com>
2021-05-14 17:31:05 -0300

    Inserindo Pedido

---
+++ src/main/java/com/rossini/cursomc/services/BoletoService.java
@@ -0,0 +1,20 @@
+package com.rossini.cursomc.services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.rossini.cursomc.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+	
+	public void preencherPagamentoComBoleto(PagamentoComBoleto pagto, Date instanteDoPedido) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(instanteDoPedido);
+		cal.add(Calendar.DAY_OF_MONTH, 7);
+		pagto.setDataVencimento(cal.getTime());
+	}
+
+}
