7fc70f6581c77654dab3047c6547fac89aa395e6
Rossini <rossyny18@gmail.com>
2021-05-23 12:15:22 -0300

    Autenticação e Autorização de Tokens (Usuarios)

--- src/main/java/com/rossini/cursomc/services/ClienteService.java
+++ src/main/java/com/rossini/cursomc/services/ClienteService.java
@@ -46,7 +46,7 @@ public class ClienteService {
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
 		obj = repo.save(obj);
-		enderecoRepository.saveAll(obj.getEndereco());
+		enderecoRepository.saveAll(obj.getEnderecos());
 		return repo.save(obj);
 	}
 	
@@ -85,7 +85,7 @@ public class ClienteService {
 		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
 		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
 		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
-		cli.getEndereco().add(end);
+		cli.getEnderecos().add(end);
 		cli.getTelefones().add(objDto.getTelefone1());
 		if(objDto.getTelefone2()!=null) {
 			cli.getTelefones().add(objDto.getTelefone2());
